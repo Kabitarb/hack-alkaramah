470fd2c64673ace7caf70b954ffb033c370ea3b1
krishna <krishnajoe7@gmail.com>
2026-02-11 11:44:28 +0400

    Up11

--- server/static.ts
+++ server/static.ts
@@ -16,7 +16,7 @@ export function serveStatic(app: Express) {
   }));
 
   // fall through to index.html if the file doesn't exist (SPA routing)
-  app.use("*", (_req, res) => {
+  app.use(/.*/, (_req, res) => {
     res.type("text/html").sendFile(path.resolve(distPath, "index.html"));
   });
 }
