49a26c961e4aeacbfa4ec3197cc07fafc6ca1512
krishna <krishnajoe7@gmail.com>
2026-02-11 11:30:37 +0400

    Up

--- server/static.ts
+++ server/static.ts
@@ -10,10 +10,13 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPath, {
+    maxAge: "1d",
+    etag: false,
+  }));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  // fall through to index.html if the file doesn't exist (SPA routing)
+  app.use("*", (_req, res) => {
+    res.type("text/html").sendFile(path.resolve(distPath, "index.html"));
   });
 }
